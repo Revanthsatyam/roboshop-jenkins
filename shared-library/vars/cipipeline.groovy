9395427cfda3170b92ef51fc7446c1b996989987
Revanthsatyam <satyamrevanth@gmail.com>
2024-02-03 11:44:01 +0530

    Terraform

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -10,6 +10,13 @@ def call() {
             }
 
             stage('Test') {
+                when {
+                    allOf {
+                        expression { env.BRANCH_NAME != null }
+                        expression { env.TAG_NAME == null }
+                    }
+                }
+
                 steps {
                     echo 'Hello World'
                 }
