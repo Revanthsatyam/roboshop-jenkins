ab72217f9652d508bdce02931196336196491aec
Revanthsatyam <satyamrevanth@gmail.com>
2024-02-04 11:00:42 +0530

    Terraform

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -2,7 +2,7 @@ def call() {
   node('workstation') {
 
     sh "find . | sed -e '1d' |xargs rm -rf"
-    git branch: 'main', url: "https://github.com/Revanthsatyam/${component}"
+    git branch: "${BRANCH_NAME}", url: "https://github.com/Revanthsatyam/${component}"
 
     stage('Compile Code') {
       common.compile()
