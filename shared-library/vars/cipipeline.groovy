656ceadd6f87513635f6fc63b0f1dea745fdd69c
Revanthsatyam <satyamrevanth@gmail.com>
2024-02-04 11:52:10 +0530

    Terraform

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -2,7 +2,15 @@ def call() {
   node('workstation') {
 
     sh "find . | sed -e '1d' |xargs rm -rf"
-    git branch: "${BRANCH_NAME}", url: "https://github.com/Revanthsatyam/${component}"
+    if(env.TAG_NAME ==~ ".*") {
+      env.branch_name = "refs/tags/${env.TAG_NAME}"
+    } else {
+      env.branch_name = "${env.BRANCH_NAME}"
+    }
+    checkout scmGit(
+        branches: [[name: 'stable-3.x']],
+        userRemoteConfigs: [[url: "https://github.com/Revanthsatyam/${component}"]]
+    )
 
     stage('Compile Code') {
       common.compile()
