989b0c1a83876d259631d544ea811fdf87a34d52
Revanthsatyam <satyamrevanth@gmail.com>
2024-02-18 17:20:17 +0530

    Terraform

--- shared-library/vars/deploy.groovy
+++ shared-library/vars/deploy.groovy
@@ -2,11 +2,21 @@ def call(){
   pipeline {
     agent any
 
+    options {
+      ansiColor('xterm')
+    }
+
+
     parameters {
       string(name: 'COMPONENT', defaultValue: '', description: 'Which Component to Deploy?')
       string(name: 'VERSION', defaultValue: '', description: 'Which Version to Deploy?')
       string(name: 'ENV', defaultValue: '', description: 'Which Environment to Deploy?')
     }
+
+    environment {
+      SSH = credentials('centos-ssh')
+    }
+
 
     stages {
 
