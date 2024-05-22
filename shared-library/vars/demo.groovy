e2aa860e8c0c521ca264cfcd7bbc5e44b6352075
Revanthsatyam <satyamrevanth@gmail.com>
2024-05-22 16:46:37 +0530

    Jenkins Jobs

--- shared-library/vars/demo.groovy
+++ shared-library/vars/demo.groovy
@@ -1,7 +1,43 @@
 def info(message) {
-    echo "INFO: ${message}"
+  echo "INFO: ${message}"
 }
 
 def warning(message) {
-    echo "WARNING: ${message}"
+  echo "WARNING: ${message}"
+}
+
+def call(){
+  pipeline {
+    agent any
+
+    stages {
+      stage('Compile') {
+        steps {
+          echo 'Hello World'
+          script {
+            demo.info 'Starting'
+            demo.warning 'Nothing to do!!'
+          }
+        }
+      }
+
+      stage('Test') {
+        steps {
+          echo 'Hello World'
+        }
+      }
+
+      stage('Code Quality') {
+        steps {
+          echo 'Hello World'
+        }
+      }
+
+      stage('Code Security') {
+        steps {
+          echo 'Hello World'
+        }
+      }
+    }
+  }
 }
