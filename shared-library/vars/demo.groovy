7cbd1be43fed748da5fd977dad19208bf3cc76fc
Revanthsatyam <satyamrevanth@gmail.com>
2024-05-24 12:14:46 +0530

    Jenkins

---
+++ shared-library/vars/demo.groovy
@@ -0,0 +1,33 @@
+def call() {
+  pipeline {
+    agent any
+
+    stages {
+
+      stage('Compile Code') {
+        steps {
+          echo 'Hello World'
+        }
+      }
+
+      stage('Test') {
+        steps {
+          echo 'Hello World'
+        }
+      }
+
+      stage('Code Quality') {
+        steps {
+          echo 'Hello World'
+        }
+      }
+
+      stage('Code Security') {
+        steps {
+          echo 'Hello World'
+        }
+      }
+
+    }
+  }
+}
