91c1f6e63cd89010a93ef200f9d1da7e4c4ba363
Revanthsatyam <satyamrevanth@gmail.com>
2024-05-25 15:10:39 +0530

    Jenkins

---
+++ shared-library/vars/common.groovy
@@ -0,0 +1,19 @@
+def compile() {
+
+  if (env.codeType == "nodejs") {
+    print 'NodeJs'
+  }
+
+  if (env.codeType == "maven")  {
+    print 'Maven'
+  }
+
+  if (env.codeType == "python") {
+    print 'Python'
+  }
+
+  if (env.codeType == "static") {
+    print 'Static'
+  }
+
+}
