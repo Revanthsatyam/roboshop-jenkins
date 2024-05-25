bbd1fb8a5474417addb31ebd4351b6bd30f0c3c9
Revanthsatyam <satyamrevanth@gmail.com>
2024-05-25 12:16:46 +0530

    Jenkins

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -1,6 +1,18 @@
 def call() {
   node('workstation') {
 
+    sh "find . | sed -e '1d' |xargs rm -rf"
+
+    if (env.TAG_NAME ==~ ".*") {
+      branch_name = "/refs/tags/${env.TAG_NAME}"
+    } else {
+      branch_name = "${env.BRANCH_NAME}"
+    }
+
+    checkout scmGit(
+      branches: [[name: branch_name]],
+      userRemoteConfigs: [[url: 'https://github.com/Revanthsatyam/${component}']])
+
     stage('Compile Code') {
       common.compile()
     }
