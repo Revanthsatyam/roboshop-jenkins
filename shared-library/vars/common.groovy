5cffc30d5deebf69ce2fed320b1b64a77e19d1b1
Revanthsatyam <satyamrevanth@gmail.com>
2024-06-02 11:11:48 +0530

    nexus

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -71,6 +71,10 @@ def release() {
       sh 'echo ${TAG_NAME} >VERSION'
       if (env.codeType == "nodejs") {
         sh 'zip -r ${component}-${TAG_NAME}.zip server.js node_modules VERSION'
+      } else if (env.codeType == "maven") {
+        sh 'cp target/${component}-1.0.jar ${component}.jar ; zip -r ${component}-${TAG_NAME}.zip ${component}.jar'
+      } else {
+        sh 'zip -r ${component}-${TAG_NAME}.zip *'
       }
 
       sh 'curl -v -u ${nexususer}:${nexuspass} --upload-file ${component}-${TAG_NAME}.zip http://172.31.35.207:8081/repository/${component}/${component}-${TAG_NAME}.zip'
