cf54f9806e85c5fd1989adfbabe3f55d3697610d
Revanthsatyam <satyamrevanth@gmail.com>
2024-06-02 10:41:18 +0530

    Jenkins

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -65,6 +65,13 @@ def codeSecurity() {
 
 def release() {
   stage('Release') {
-    print 'Release'
+    env.nexususer = sh (script: 'aws ssm get-parameter --name "nexus.username" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
+    env.nexuspass = sh (script: 'aws ssm get-parameter --name "nexus.password" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
+    wrap([$class: "MaskPasswordsBuildWrapper", varPasswordPairs: [[password: nexuspass]]]) {
+      sh 'echo ${TAG_NAME} >VERSION'
+      if (env.codeType == "nodejs") {
+        sh 'zip -r ${component}-${TAG_NAME}.zip server.js node_modules VERSION'
+      }
+    }
   }
 }
